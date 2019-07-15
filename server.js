c17fa6a4423b9d3c1c944f67ee24df5a2ecc757b
Shannon <shannon.yoshikawa@gmail.com>
2019-07-15 12:54:15 -0700

    queries done, API requests written but not tested

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const db = require('./data/dbConfig.js');
+const AccountsRouter = require("./data/accounts-router")
 
 const server = express();
 
 server.use(express.json());
+server.use('/accounts', AccountsRouter)
+
+server.get('/', (req, res)=> {
+    res.send(`<h3>Server Running</h3>`)
+})
 
 module.exports = server;
